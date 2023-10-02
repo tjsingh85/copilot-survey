a1ab5d4ec101529af63451743115c7cbc42809a1
tjsingh85 <tjsingh85@github.com>
2023-10-02 16:37:39 +0200

    added actions and labeler yml

---
+++ fetch-copilot-survey.py
@@ -0,0 +1,62 @@
+import requests
+import pandas as pd
+import re
+
+def get_pull_requests(org_name, label, token):
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': f'token {token}'
+    }
+
+    repos_url = f"https://api.github.com/orgs/{org_name}/repos"
+    repos = requests.get(repos_url, headers=headers).json()
+
+    prs_with_label = []
+    for repo in repos:
+        prs_url = f"https://api.github.com/repos/{org_name}/{repo['name']}/pulls"
+        prs = requests.get(prs_url, headers=headers).json()
+        for pr in prs:
+            labels = [label['name'] for label in pr['labels']]
+            if label in labels:
+                prs_with_label.append(pr)
+
+    return prs_with_label
+
+# Usage
+pull_requests = get_pull_requests('org-name', 'label', 'token')
+
+# Create an empty DataFrame
+df = pd.DataFrame(columns=['PR URL', 'Org', 'Repo', 'Creator', 'Used Copilot', 'Time Saved', 'Used Frequency', 'Continue Use'])
+
+for pr in pull_requests:
+    org = pr['base']['repo']['owner']['login']
+    pr_url = pr['html_url']
+    pr_repo = pr['head']['repo']['name']
+    creator = pr['user']['login']
+    data = pr['body']
+
+    questions_and_options = re.findall(r'\*\*\*(.*?)\*\*\*(.*?)(?=\*\*\*|$)', data, re.DOTALL)
+    answers = {}
+
+    for question, options in questions_and_options:
+        question = question.strip()
+        selected_option = re.search(r'- \[X\] (.*?)\n', options)
+        if selected_option:
+            answers[question] = selected_option.group(1)
+        else:
+            answers[question] = "N/A"
+
+    # Append the data to the DataFrame
+    df = df._append({
+        'PR URL': pr_url,
+        'Org': org,
+        'Repo': pr_repo,
+        'Creator': creator,
+        'Used Copilot': answers.get("Did you use Copilot in this PR?", "N/A"),
+        'Time Saved': answers.get("How much time(in minutes) did you save by using Copilot?", "N/A"),
+        'Used Frequency': answers.get("For this PR, Copilot allowed me to spend less time searching external resources?", "N/A"),
+        'Continue Use': answers.get("I felt more productive using Copilot for this PR?", "N/A")
+    }, ignore_index=True)
+
+# Export the DataFrame to an Excel file
+df.to_excel('pull_requests.xlsx', index=False)
---
+++ pull_request_template.md
@@ -0,0 +1,19 @@
+### Copilot Usage Survey
+
+1. ***Did you use Copilot in this PR?***
+- [ ] No
+- [ ] Yes
+
+2. ***How much time(in minutes) did you save by using Copilot?***
+- [ ] < 1
+- [ ] 1-5
+- [ ] 5-10
+- [ ] >10
+
+3. ***For this PR, Copilot allowed me to spend less time searching external resources?***
+- [ ] No
+- [ ] Yes
+      
+4. ***I felt more productive using Copilot for this PR?*** 
+- [ ] No
+- [ ] Yes
