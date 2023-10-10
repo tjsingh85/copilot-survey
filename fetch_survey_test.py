3c9304d4d9833f5801566661eb894d0176acec3c
Tajinder Singh (TJ) <tjsingh85@github.com>
2023-10-10 12:27:53 +0000

    added few tests

--- fetch_survey_test.py
+++ fetch_survey_test.py
@@ -1,43 +1,57 @@
 import unittest
-import fetch_survey as fcs
+import fetch_survey as fs
 
 class TestFetchCopilotSurvey(unittest.TestCase):
 
     def test_get_selected_option(self):
         options = "- [X] Answer1\n- [ ] Answer2"
-        result = fcs.get_selected_option(options)
+        result = fs.get_selected_option(options)
         self.assertEqual(result, 'Answer1')
 
     def test_get_selected_option_with_prefix_space(self):
         options = "- [ X] Answer1\n- [ ] Answer2"
-        result = fcs.get_selected_option(options)
+        result = fs.get_selected_option(options)
         self.assertEqual(result, 'Answer1')
     
     def test_get_selected_option_with_suffix_space(self):
         options = "- [X ] Answer1\n- [ ] Answer2"
-        result = fcs.get_selected_option(options)
+        result = fs.get_selected_option(options)
         self.assertEqual(result, 'Answer1')
 
     def test_get_selected_option_with_spaces(self):
         options = "- [ X ] Answer1\n- [ ] Answer2"
-        result = fcs.get_selected_option(options)
+        result = fs.get_selected_option(options)
         self.assertEqual(result, 'Answer1')
     
     def test_get_first_selected_options(self):
         options = "- [ X ] Answer1\n- [X] Answer2"
-        result = fcs.get_selected_option(options)
+        result = fs.get_selected_option(options)
         self.assertEqual(result, 'Answer1')
     
     def test_get_second_selected_options(self):
         options = "- [ ] Answer1\n- [X] Answer2\n- [ ] Answer3"
-        result = fcs.get_selected_option(options)
+        result = fs.get_selected_option(options)
         self.assertEqual(result, 'Answer2')
 
     def test_get_NA_for_no_selected_options(self):
         options = "- [ ] Answer1\n- [ ] Answer2\n- [ ] Answer3"
-        result = fcs.get_selected_option(options)
+        result = fs.get_selected_option(options)
         self.assertEqual(result, 'N/A')
 
+    def test_get_answers(self):
+        data = "***Question***\n- [X] Answer\n***"
+        result = fs.get_answers(data)
+        self.assertEqual(result, {'Question': 'Answer'})
+    
+    def test_get_NA_as_answer_when_nothing_is_selected(self):
+        data = "***Question***\n- [ ] Answer\n***"
+        result = fs.get_answers(data)
+        self.assertEqual(result, {'Question': 'N/A'})
+
+    def test_get_first_answer_if_mulitple_answers_are_selected(self):
+        data = "***Question***\n- [X] Answer1\n- [X] Answer2***"
+        result = fs.get_answers(data)
+        self.assertEqual(result, {'Question': 'Answer1'})
 
 if __name__ == '__main__':
     unittest.main()
